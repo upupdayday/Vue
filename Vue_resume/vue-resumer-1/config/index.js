d28b079a5a1bee9d40378c4b43c0d2f668ec6a37
upupdayday <sunhanqing1987@163.com>
2018-09-14 05:16:42 +0800

    modify config && replace dist

--- Vue_resume/vue-resumer-1/config/index.js
+++ Vue_resume/vue-resumer-1/config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/Vue/Vue_resume/vue-resumer-1/dist',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/Vue/Vue_resume/vue-resumer-1/dist',
 
     /**
      * Source Maps
