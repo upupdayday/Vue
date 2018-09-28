3c445f409dc0dae38a1a58554e6f3bc28bbda80f
upupdayday <sunhanqing1987@163.com>
2018-09-29 04:25:07 +0800

    封装工作经历组件

--- Vue_resume/vue-resumer-1/src/components/Editor.vue
+++ Vue_resume/vue-resumer-1/src/components/Editor.vue
@@ -17,23 +17,7 @@
               <Profile v-bind:profile="profile"/>
             </li>
             <li :class="{active: currentTab === 1}">
-              <h2>工作经历</h2>
-              <el-form>
-                <div class="workWrapper" v-for="(work, index) in workExperience">
-                  <el-button @click="deleteWork(index)" type="danger" icon="el-icon-delete" size="small"></el-button>
-                  <el-form-item label="公司">
-                        <el-input v-model="work.company"></el-input>
-                    </el-form-item>
-                    <el-form-item label="职位">
-                        <el-input v-model="work.position"></el-input>
-                    </el-form-item>
-                    <el-form-item label="工作内容">
-                        <el-input v-model="work.content"></el-input>
-                  </el-form-item>                  
-                  <hr>
-                </div>
-                <el-button @click="addWork" type="primary" plain>添加经历</el-button>
-              </el-form>
+              <WorkHistory v-bind:workExperience="workExperience"/>
             </li>
       </ol>
     </div>
@@ -42,8 +26,9 @@
 
 <script>
 import Profile from './Profile'
+import WorkHistory from './WorkHistory'
 export default {
-  components: {Profile},
+  components: {Profile, WorkHistory},
   data() {
     return {
       currentTab: 0,
@@ -63,14 +48,7 @@ export default {
     }
   },
   methods:{
-    addWork: function(){
-      this.workExperience.push({company:'',
-                                position:'',
-                                content:''})
-    },
-    deleteWork: function(index){
-      this.workExperience.splice(index,1)
-    },
+
   }
 }
 </script>
---
+++ Vue_resume/vue-resumer-1/src/components/WorkHistory.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h2>工作经历</h2>
+        <el-form>
+        <div class="workWrapper" v-for="(work, index) in workExperience">
+            <el-button @click="deleteWork(index)" type="danger" icon="el-icon-delete" size="small"></el-button>
+            <el-form-item label="公司">
+                <el-input v-model="work.company"></el-input>
+            </el-form-item>
+            <el-form-item label="职位">
+                <el-input v-model="work.position"></el-input>
+            </el-form-item>
+            <el-form-item label="工作内容">
+                <el-input v-model="work.content"></el-input>
+            </el-form-item>                  
+            <hr>
+        </div>
+        <el-button @click="addWork" type="primary" plain>添加经历</el-button>
+        </el-form>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['workExperience'],
+          methods:{
+        addWork: function(){
+        this.workExperience.push({company:'',
+                                    position:'',
+                                    content:''})
+        },
+        deleteWork: function(index){
+        this.workExperience.splice(index,1)
+        },
+  }
+    }
+</script>
