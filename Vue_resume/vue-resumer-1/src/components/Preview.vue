9e9701d2c761b2a8593361b5ee79b36c94ca538a
upupdayday <sunhanqing1987@163.com>
2018-10-16 05:03:11 +0800

    增加css样式

--- Vue_resume/vue-resumer-1/src/components/Preview.vue
+++ Vue_resume/vue-resumer-1/src/components/Preview.vue
@@ -1,37 +1,34 @@
 <template>
     <div id="preview">
-      <section v-show="resume.profile">
+      <section data-name="profile" v-show="resume.profile">
           <h1>{{resume.profile.itemData[0].name}}</h1>
           <h2>{{resume.profile.title}}</h2>
-          <p>
-              {{resume.profile.itemData[0].city}} {{resume.profile.itemData[0].birth}}
-          </p>
+          <p>城市：{{resume.profile.itemData[0].city}}</p>
+          <p>出生日期：{{resume.profile.itemData[0].birth}}</p>
       </section>
 
-      <section v-show="resume.work">
+      <section data-name="work" v-show="resume.work">
           <h2>{{resume.work.title}}</h2>
           <ol>
               <li v-for="item in resume.work.itemData">
-                  <h3>{{item.company}}</h3>
-                  <h4>{{item.duration}}</h4>
+                  <h3>{{item.company}}  {{item.duration}}</h3>
                   <p>{{item.content}}</p>
               </li>
           </ol>
       </section>
 
-      <section v-show="resume.study">
+      <section data-name="study" v-show="resume.study">
           <h2>{{resume.study.title}}</h2>
           <ol>
               <li v-for="item in resume.study.itemData">
-                  <h3>{{item.school}}</h3>
-                  <h4>{{item.duration}}</h4>
-                  <p>{{item.major}}</p>
-                  <p>{{item.degree}}</p>
+                  <h3>{{item.school}}   {{item.duration}}</h3>
+                  <p>专业：{{item.major}}</p>
+                  <p>学位：{{item.degree}}</p>
               </li>
           </ol>
       </section>
 
-      <section v-show="resume.project">
+      <section data-name="project" v-show="resume.project">
           <h2>{{resume.project.title}}</h2>
           <ol>
               <li v-for="item in resume.project.itemData">
@@ -41,7 +38,7 @@
           </ol>
       </section>
 
-      <section v-show="resume.reward">
+      <section data-name="reward" v-show="resume.reward">
           <h2>{{resume.reward.title}}</h2>
           <ol>
               <li v-for="item in resume.reward.itemData">
@@ -51,7 +48,7 @@
           </ol>
       </section>
 
-      <section v-show="resume.contact">
+      <section data-name="contact" v-show="resume.contact">
           <h2>{{resume.contact.title}}</h2>
           <ol>
               <li v-for="(value,key) in resume.contact.itemData[0]">
@@ -85,5 +82,40 @@
     height: 925px; */
     box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
     overflow:auto;
+    padding: 2em;
+    line-height: 1.2;
+    color: #333;
+    *{box-sizing: border-box; font-variant: normal; font-weight: normal;}
+    ol{ list-style: none; }
+    p{ white-space: pre-line; } // 去除用户输入中的空格，保留换行
+
+    section + section{ margin-top: 2em; }/*设置section间距*/
+    
+    section{
+        >h2{
+            width: 100%;
+            background: #409EFF; 
+            display: inline-block;
+            padding: .2em;
+            margin-bottom:.5em;
+        }
+        li + li { margin-top: 1em; }
+        li{
+            line-height: 1.5;
+            h3{ border-bottom: 1px solid #999; padding-bottom: .3em; margin-bottom: .3em; }
+        }
+    }
+
+    section[data-name="profile"]{
+        >h1{
+            margin: .1em 0; 
+            font-size:3em;
+        }    
+    }
+
+
+
+
+    
   }
 </style>
