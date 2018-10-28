3a1c9a5319d29387f6b8b4bfc1c8e63ae1a0865c
upupdayday <sunhanqing1987@163.com>
2018-10-29 04:26:38 +0800

    安装leancloud，构建初始化模块

---
+++ Vue_resume/vue-resumer-1/src/lib/leancloud.js
@@ -0,0 +1,8 @@
+import AV from 'leancloud-storage'
+var APP_ID = 'q1m18i6vGWFPNKyecoKuo4OW-gzGzoHsz';
+var APP_KEY = 'DglvfX7JwRpd4j4spbAeM4ss';
+AV.init({
+  appId: APP_ID,
+  appKey: APP_KEY
+});
+ export default AV
