7c18419e9facf5e86cd5a12fc0fdb19d96e28888
upupdayday <sunhanqing1987@163.com>
2018-10-30 05:45:06 +0800

    登出功能实现

--- Vue_resume/vue-resumer-1/src/store/index.js
+++ Vue_resume/vue-resumer-1/src/store/index.js
@@ -160,6 +160,9 @@ export default new Vuex.Store({
         setUser(state, payload) {
             Object.assign(state.user, payload)
                 // console.log(state.user)
+        },
+        removeUser(state) {
+            state.user.id = null
         }
     }
 })
